d452a06c858065a78a7c5243837bea8957629f0d
simurg12 <65874751+simurg12@users.noreply.github.com>
2022-03-09 15:02:44 +0300

    Update Version

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import subprocess
 import optparse
+import re
 
-parse_object = optparse.OptionParser()
-parse_object.add_option("-i","--interface",dest="intarface",help="Entery your interface")
-parse_object.add_option("-m","--mac_address",dest="mac_address", help="Entry your want it mac address")
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--interface",dest="interface",help="Entery your interface")
+    parse_object.add_option("-m","--mac_address",dest="mac_address", help="Entry your want it mac address")
 
-(user_inputs,argument) = parse_object.parse_args()
+    return parse_object.parse_args()
 
-user_interface = user_inputs.interface
-user_mac_adress= user_inputs.mac_adress
+def change_mac_adress(user_interface,user_mac_address):
+    subprocess.call(["ifconfig",user_interface,"down"])
+    subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
+    subprocess.call(["ifcongif",user_interface,"up"])
 
-subprocess.call(["ifconfig",user_interface,"down"])
-subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_adress])
-subprocess.call(["ifcongif",user_interface,"up"])
+def control_new_mac(interface):
+
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig)
+
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
+
+print ("MacChanger Started!")
+(user_inputs,argument) = get_user_input()
+change_mac_adress(user_inputs.interfaces,user_inputs.mac_address)
+finalized_mac = control_new_mac(user_inputs.interfaces)
+
+if finalized_mac == user_inputs.mac_address:
+    print("success")
+else:
+    print("Error!!")
